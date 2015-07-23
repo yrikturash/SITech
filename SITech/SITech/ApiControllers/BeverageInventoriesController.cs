40d291b8d7d7a22c2bbf74eafc79ce79a374fc9c
Turash Jurij <yrik.turash@gmail.com>
2015-07-23 10:56:23 +0300

    create BeverageInventoriesController.cs

---
+++ SITech/SITech/ApiControllers/BeverageInventoriesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SITech.DTO;
+using SITech.Models;
+
+namespace SITech.ApiControllers
+{
+    [System.Web.Http.RoutePrefix("api/beverageInventories")]
+    public class BeverageInventoriesController : ApiController
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public BeverageInventoriesController()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("add")]
+        public HttpResponseMessage AddMenuItem([FromBody] BeverageInventoryViewModel model)
+        {
+            try
+            {
+                _unitOfWork.BeverageInventories.Create(model);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
